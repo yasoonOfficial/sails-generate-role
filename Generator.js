dda1b95501d6fed66cc30f0e6af1cb5900a7f760
tobiasviehweger <tobias.viehweger@gmx.de>
2014-12-12 10:09:03 +0100

    fixes

--- Generator.js
+++ Generator.js
@@ -4,7 +4,8 @@
 
 var util = require('util');
 var _ = require('lodash');
-var roleFunc = require('./index');
+var indexFunc = require('./index');
+var funcs = require('./lib/functions');
 _.defaults = require('merge-defaults');
 
 
@@ -20,8 +21,6 @@ _.defaults = require('merge-defaults');
 
 var generator = {
 
-  rolePolicy: roleFunc,
-
   /**
    * `before()` is run before executing any of the `targets`
    * defined below.
@@ -32,7 +31,9 @@ var generator = {
    * @param  {Object} scope
    * @param  {Function} cb    [callback]
    */
-    
+  rolePolicy: indexFunc,
+  getRestrictedFindOne: funcs.getRestrictedFindOne,
+  
   before: function (scope, cb) {
 
     // scope.args are the raw command line arguments.
@@ -61,14 +62,10 @@ var generator = {
     });
 
     //Check if ContextRole exists yet
-    var roleCtxPath = require('path').resolve(scope.rootPath, './api/roles/RoleContext.js');
-    if (!require('fs').existsSync(roleCtxPath))
+    var roleCtxPath = require('path').resolve(scope.rootPath, './api/roles/RoleContext.js')
+    if (!require('fs').existsSync())
         generator.targets['./api/roles/RoleContext.js'] = { copy: 'RoleContext.js' }
 
-	var hookPath = require('path').resolve(scope.rootPath, './api/hooks/role/index.js');
-	if (!require('fs').existsSync(hookPath)) 
-		generator.targets['./api/hooks/role/index.js'] = { copy: 'hookShell.js' };
-		
     // Decide the output filename for use in targets below:
     scope.fileName = scope.args[0] + 'Role';
 
@@ -98,7 +95,8 @@ var generator = {
     //
     // The `template` helper reads the specified template, making the
     // entire scope available to it (uses underscore/JST/ejs syntax).
-    // Then the file is copied into the specified destination (on the left).    
+    // Then the file is copied into the specified destination (on the left).
+    './api/hooks/role/index.js': { copy: 'hookShell.js' },
     './api/roles/:fileName.js': { template: 'role.js' }
   },
 
